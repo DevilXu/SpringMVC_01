b29e9e7d5ddbf0ede6c7f55aadc75a7862cf122e
DevilXu <a630975128@126.com>
2014-09-01 17:15:04 +0800

    JSON+JUMP

--- src/org/swinglife/controller/action/UserController.java
+++ src/org/swinglife/controller/action/UserController.java
@@ -4,15 +4,17 @@ package org.swinglife.controller.action;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.common.base.AbstractBaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.swinglife.controller.bean.demo;
 import org.swinglife.controller.service.DemoService;
 @Controller
-public class UserController {
+public class UserController extends AbstractBaseController{
 	@Autowired   
 	private demo demo=new demo();
 	@Autowired   
@@ -25,11 +27,22 @@ public class UserController {
 			mav.addObject("username",username);
 			mav.addObject("password", password);
 			listDemo=demoService.getUserByUid();
+			mav.addObject("listDemo", listDemo);
 			return mav;
 		}
-		return new ModelAndView("home");
+		return new ModelAndView("index");
 	}
-
+	@RequestMapping(value="page/jump")
+	public ModelAndView jump(String username,String password){
+		return new ModelAndView("demo/index");
+	}
+	 
+	@RequestMapping(value="page/getList")
+	@ResponseBody 
+	public void getList() throws Exception {  
+		listDemo=demoService.getUserByUid();
+		this.outputAjaxJsonData(listDemo);
+	}  
 	/***
 	 * ��֤����
 	 * @param params
